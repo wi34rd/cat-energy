a29bf0637fc5c179d2c73f1ab48ac6f6ec388b62
wi34rd <wi34rd@gmail.com>
2019-02-03 15:15:05 +0700

    Initial commit with index mobile page markup

---
+++ gulpfile.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const path = require('path');
+
+const browserSync = require('browser-sync');
+const del = require('del');
+const nunjucks = require('nunjucks');
+
+const gulp = require('gulp');
+const data = require('gulp-data');
+const gulpNunjucks = require('gulp-nunjucks');
+// const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+
+
+gulp.task('clean', () => {
+    return del('build/')
+});
+
+gulp.task('html', () => {
+    return gulp.src('src/html/pages/**/*.html')
+        .pipe(data((file) => Object.assign(
+            JSON.parse(fs.readFileSync('src/html/site.json')),
+            JSON.parse(fs.readFileSync(path.join(path.dirname(file.path), path.basename(file.path, '.html') + '.json')))
+        )))
+        .pipe(gulpNunjucks.compile(undefined, {
+            env: new nunjucks.Environment(new nunjucks.FileSystemLoader(['src/html/base/', 'src/html/blocks/']))
+        }))
+        .pipe(gulp.dest((file) => {
+            file.path = path.join(file.base, path.basename(file.path));
+
+            return 'build/';
+        }));
+});
+
+gulp.task('css', () => {
+    return gulp.src('src/sass/styles.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('build/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('other', () => {
+    return gulp.src([
+            'src/fonts/*.{svg,ttf,woff,woff2}',
+            'src/images/**/*.{svg,jpg,jpeg,png}',
+            '!src/images/inline/*',
+            '!src/images/inline',
+            '!src/images/sprites/*',
+            '!src/images/sprites',
+        ], {
+            base: 'src/'
+        })
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('reload', done => {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('build', gulp.series('clean', 'other', 'css', 'html'));
+
+gulp.task('serve', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'build/'
+        },
+        browser: '%LOCALAPPDATA%/Google/Chrome SxS/Application/chrome.exe'
+    });
+
+    // gulp.watch('src/images/sprites/*.svg', gulp.series('svgstore'));
+
+    gulp.watch(
+        ['src/html/**', 'src/images/inline/*'],
+        gulp.series('html', 'reload')
+    );
+
+    gulp.watch('src/sass/**/*.{scss,sass}', gulp.series('css')); // TODO: Добавить задачу "csso"
+
+    // gulp.watch('src/js/**/*.js', gulp.series('uglify'));
+
+    gulp.watch(
+        [
+            'src/fonts/**/*.{svg,ttf,woff,woff2}',
+            'src/images/**/*.{svg,jpg,jpeg,png}'
+        ],
+        gulp.series('other', 'reload')
+    );
+
+    // gulp.watch(
+    //     ['src/images/**/*.svg', '!src/images/inline/*', '!src/images/sprites/*'],
+    //     gulp.series('svgo', 'reload')
+    // );
+});
