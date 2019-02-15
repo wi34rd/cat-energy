c2be2316021f32cc6bcc18626bc4cc7e6e9944a1
wi34rd <wi34rd@gmail.com>
2019-02-15 19:12:33 +0700

    Удаляет папку build, меняет иконку гамбургера на SVG

--- build/js/main.js
+++
@@ -1,5 +0,0 @@
-document.body.classList.toggle('nojs');
-document.getElementById('drawer-button').addEventListener('click', function () {
-    document.getElementById('main-menu').classList.toggle('navbar__menu--collapsed');
-    this.classList.toggle('drawer-button--close');
-});
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 document.body.classList.toggle('nojs');
 document.getElementById('drawer-button').addEventListener('click', function () {
+    const isExpanded = this.getAttribute('aria-expanded');
+    this.setAttribute('aria-expanded', isExpanded !== 'true' || false);
+
     document.getElementById('main-menu').classList.toggle('navbar__menu--collapsed');
-    this.classList.toggle('drawer-button--close');
 });
